757e43b97d4bbb91543522b54639957034e78de8
Yash Shah <yashshh123@gmail.com>
2022-05-15 17:16:20 +0530

    NFT collection webisite
    NFT collection webisite

---
+++ smart-contract/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x6fa529508f506E7D7D2bC51454f4956C048aD7d5";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
